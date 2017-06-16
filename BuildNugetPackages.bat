1d01baf8a169c1c88a61afbfb30c1ed4b70adfbc
Travis Taylor <codenesium@gmail.com>
2017-06-16 16:43:01 -0500

    updated nuget files

--- BuildNugetPackages.bat
+++ BuildNugetPackages.bat
@@ -1,2 +1,2 @@
-nuget pack IISIntegration/IISIntegration.csproj  -IncludeReferencedProjects  -o  ../NugetPackages
+nuget pack IISIntegration/IISIntegration.csproj  -Version 2017.1.1  -o  ../NugetPackages
 pause
