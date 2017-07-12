7e073c97c02ddff1c77bdec8f476a2ec6a44ce16
Travis Taylor <codenesium@gmail.com>
2017-07-12 10:03:29 -0500

    Updated nuget config

--- BuildNugetPackages.bat
+++ BuildNugetPackages.bat
@@ -1,2 +1,2 @@
-nuget pack IISIntegration/IISIntegration.nuspec  -o  ../NugetPackages
+nuget pack IISIntegration/IISIntegration.nuspec -Prop Configuration=Release -Version 2017.1.1  -o  ../NugetPackages
 pause
